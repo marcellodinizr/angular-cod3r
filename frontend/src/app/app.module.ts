c5810a033fd53cb22ba24c5ebadb8c1f2897ff16
marcellodinizr <marcellodinizrocha@gmail.com>
2021-07-16 11:52:15 -0300

    addind new imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./components/template/header/header.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	declarations: [AppComponent, HeaderComponent, FooterComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
