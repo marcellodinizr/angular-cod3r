364ea357408f9a7c7e1f79322e38dbf89d7ff218
marcellodinizr <marcellodinizrocha@gmail.com>
2021-07-18 17:14:11 -0300

    general commits and new imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,17 +3,32 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HeaderComponent } from "./components/template/header/header.component";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { FooterComponent } from "./components/template/footer/footer.component";
-import { NavComponent } from "./components/template/nav/nav.component";
 
+import { HeaderComponent } from "./components/template/header/header.component";
+import { NavComponent } from "./components/template/nav/nav.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from "@angular/material/list";
 
+import { HomeComponent } from "./views/home/home.component";
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { RedDirective } from "./directives/red.directive";
+
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		FooterComponent,
+		NavComponent,
+		HomeComponent,
+		ProductCrudComponent,
+		RedDirective,
+	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
@@ -21,6 +36,7 @@ import { MatListModule } from "@angular/material/list";
 		MatToolbarModule,
 		MatSidenavModule,
 		MatListModule,
+		MatCardModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
